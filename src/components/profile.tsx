c10e6a4a520d4e334576758159d5a7c9bd7d2b21
Carlos Vinicius <69722024+CarlossViniciuss@users.noreply.github.com>
2021-02-28 20:07:26 -0300

    Initial commit

---
+++ src/components/profile.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://avatars.githubusercontent.com/u/69722024?s=460&u=6f1ae931d311e3063bae177286efc2811245a1e8&v=4" alt="" />
+            <div>
+                <strong>Carlos Vinicius</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level" />
+                    Level {level}
+                </p>
+            </div>
+        </div>
+    );
+}
